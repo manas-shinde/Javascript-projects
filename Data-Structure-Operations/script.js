f26424275935c3db8b2e388f9cfc7eb9b059774d
manas-shinde <manas.a.shinde11@gmail.com>
2023-01-29 16:45:00 +0530

    feat : challenge 1 completed.

--- Data-Structure-Operations/script.js
+++ Data-Structure-Operations/script.js
@@ -55,3 +55,81 @@ console.log(weekDays);
 
 //REST Function
 restaurant.orderPizza('Panner', 'onions', 'mushrooms', 'chesse');
+
+/**
+ * ///////////////////////////////////////////////////
+ * Coding Challenge - 1
+ */
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+//1
+const [player1, player2] = game.players;
+
+//2
+const [gk, ...fieldPlayers] = [...player1];
+console.log(gk);
+console.log(fieldPlayers);
+
+//3
+const allPlayers = [...player1, ...player2];
+console.log(allPlayers);
+
+//4
+const player1final = [...player1, 'Thiago', 'coutioh', 'Persitic'];
+console.log(player1final);
+
+//5
+const {
+  odds: { team1, x, team2 },
+} = game;
+console.log(team1, x, team2);
+
+//6
+let printGoals = (...players) => {
+  console.log(`Total ${players.length} goal scored!!`);
+};
+
+printGoals(...game.scored);
+
+//7
+team1 < team2 && console.log('Team 1 Win the game !!');
+team1 > team2 && console.log('Team 2 Win the game !!');
