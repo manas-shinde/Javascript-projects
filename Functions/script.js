acd9f8f2db29ceffbbcc6fbc20379c885df21202
manas-shinde <manas.a.shinde11@gmail.com>
2023-04-02 03:57:10 +0530

    worked with Higher Order Function.

--- Functions/script.js
+++ Functions/script.js
@@ -26,3 +26,25 @@ createNewBooking('LH123');
 createNewBooking('LH123', 3);
 createNewBooking('LH123', 4, 200);
 createNewBooking('LH123', undefined, 500);
+
+/////////// Higher Order Functions ///////////
+
+const oneWord = str => {
+  return str.replace(/ /g, '').toLowerCase();
+};
+const upperFirstWord = str => {
+  let [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher order functions
+const transformer = (str, fun) => {
+  console.log(`Original String :${str}`);
+
+  console.log(`After tranformation : ${fun(str)}`);
+
+  console.log(`Transformation Function used : ${fun.name}`);
+};
+
+transformer('Javascript is good !', oneWord);
+transformer('Java is better.', upperFirstWord);
