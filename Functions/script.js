2e15aff4057a2a761c1761dba433f4b5744ebb2f
manas-shinde <manas.a.shinde11@gmail.com>
2023-04-01 11:14:43 +0530

    set default values to function parameters.

--- Functions/script.js
+++ Functions/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+///////////  Default Parameter ///////////
+const bookings = [];
+
+const createNewBooking = function (
+  flightNum,
+  NumPassangers = 1,
+  price = 199 * NumPassangers
+) {
+  // Old ES5
+  //   NumPassangers = NumPassangers || 1;
+  //   price = price || 199;
+
+  let booking = {
+    flightNo: flightNum,
+    NumPassangers: NumPassangers,
+    price,
+  };
+
+  console.table(booking);
+  bookings.push(booking);
+};
+
+createNewBooking('LH123');
+createNewBooking('LH123', 3);
+createNewBooking('LH123', 4, 200);
+createNewBooking('LH123', undefined, 500);
