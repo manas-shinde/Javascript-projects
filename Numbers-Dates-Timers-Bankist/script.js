8054e8e8e1e7e614094cef4e666a68772914f130
manas-shinde <manas.a.shinde11@gmail.com>
2023-04-13 18:05:21 +0530

    feat : Implemented a feature for transaction how many days are passed.

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -16,14 +16,14 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2022-11-18T21:31:17.178Z',
+    '2022-12-23T07:42:02.383Z',
+    '2023-01-28T09:15:04.904Z',
+    '2023-02-01T10:17:24.185Z',
+    '2023-03-08T14:11:59.604Z',
+    '2023-04-07T17:01:17.194Z',
+    '2023-04-11T23:36:17.929Z',
+    '2023-04-12T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -36,14 +36,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2022-11-01T13:15:33.035Z',
+    '2022-11-30T09:48:16.867Z',
+    '2022-12-25T06:04:23.907Z',
+    '2023-01-25T14:18:46.235Z',
+    '2023-02-05T16:33:06.386Z',
+    '2023-04-10T14:43:26.374Z',
+    '2023-04-11T18:49:59.371Z',
+    '2023-04-12T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -81,6 +81,20 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const displayMovementsWithDaysPassed = (date1, date2) => {
+  let daysPassed = Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  if (daysPassed === 0) return `Today`;
+  else if (daysPassed === 1) return 'Yesterday';
+  else if (daysPassed > 1) return `${daysPassed} days`;
+};
+
+const displayStandardDate = date => {
+  let day = `${date.getDate()}`.padStart(2, '0');
+  let month = `${date.getMonth()}`.padStart(2, '0');
+  let year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -92,12 +106,10 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     let date = new Date(acc.movementsDates[i]);
-    let day = `${date.getDate()}`.padStart(2, '0');
-    let month = `${date.getMonth()}`.padStart(2, '0');
-    let year = date.getFullYear();
 
-    let displayDate = `${day}/${month}/${year}`;
+    // let displayDate = displayStandardDate(date);
 
+    let displayDate = displayMovementsWithDaysPassed(new Date(), date);
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
