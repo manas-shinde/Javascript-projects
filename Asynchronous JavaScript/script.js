44a5365565060ac79d9e0b9d6e7251717ec9d1dc
manas-shinde <manas.a.shinde11@gmail.com>
2023-04-30 18:21:50 +0530

    Coding challenge #1 .

--- Asynchronous JavaScript/script.js
+++ Asynchronous JavaScript/script.js
@@ -54,29 +54,98 @@ const renderCountry = data => {
 // getCountryData('India');
 // getCountryData('Portugal');
 
-const fetchData = (url, errMsg = 'Something went wrong!') => {
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(` ${errMsg} : ${response.status}`);
-    }
+// const fetchData = (url, errMsg = 'Something went wrong!') => {
+//   return fetch(url).then(response => {
+//     if (!response.ok) {
+//       throw new Error(` ${errMsg} : ${response.status}`);
+//     }
 
-    return response.json();
-  });
-};
+//     return response.json();
+//   });
+// };
 
-const getCountryData = country => {
-  fetchData(
-    `https://restcountries.com/v3.1/name/${country}`,
-    'Country not found'
+// const getCountryData = country => {
+//   fetchData(
+//     `https://restcountries.com/v3.1/name/${country}`,
+//     'Country not found'
+//   )
+//     .then(data => renderCountry(data[0]))
+//     .catch(err => {
+//       renderError(`Something went wrong : ${err.message}`);
+//       console.log(err);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// getCountryData('manas');
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+
+const whereAmI = (lat, lng) => {
+  fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
   )
-    .then(data => renderCountry(data[0]))
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+
+      let msg = `You are in ${data.locality}, ${data.principalSubdivision}(${data.countryName})`;
+      console.log(msg);
+      countriesContainer.insertAdjacentText('beforeend', msg);
+    })
     .catch(err => {
-      renderError(`Something went wrong : ${err.message}`);
-      console.log(err);
+      console.error(err.message);
+      renderError(err.message);
     })
     .finally(() => {
       countriesContainer.style.opacity = 1;
     });
 };
 
-getCountryData('manas');
+btn.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        const { latitude, longitude } = position.coords;
+        console.log(latitude, longitude);
+
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+        whereAmI(latitude, longitude);
+      },
+      function () {
+        alert(`Couldn't able to get your location`);
+      }
+    );
+  }
+});
