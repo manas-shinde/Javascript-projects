5742a9cbe54269e9979dd4f1551567e026776e43
manas-shinde <manas.a.shinde11@gmail.com>
2023-05-01 10:33:33 +0530

    Coding challenge #2 completed.

--- Asynchronous JavaScript/script.js
+++ Asynchronous JavaScript/script.js
@@ -108,44 +108,111 @@ TEST COORDINATES 2: -33.933, 18.474
 GOOD LUCK 😀
 */
 
-const whereAmI = (lat, lng) => {
-  fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
-  )
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
+// const whereAmI = (lat, lng) => {
+//   fetch(
+//     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
+//   )
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data);
 
-      let msg = `You are in ${data.locality}, ${data.principalSubdivision}(${data.countryName})`;
-      console.log(msg);
-      countriesContainer.insertAdjacentText('beforeend', msg);
-    })
-    .catch(err => {
-      console.error(err.message);
-      renderError(err.message);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
+//       let msg = `You are in ${data.locality}, ${data.principalSubdivision}(${data.countryName})`;
+//       console.log(msg);
+//       countriesContainer.insertAdjacentText('beforeend', msg);
+//     })
+//     .catch(err => {
+//       console.error(err.message);
+//       renderError(err.message);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// btn.addEventListener('click', e => {
+//   e.preventDefault();
+
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(
+//       position => {
+//         console.log(position);
+//         const { latitude, longitude } = position.coords;
+//         console.log(latitude, longitude);
+
+//         console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+//         whereAmI(latitude, longitude);
+//       },
+//       function () {
+//         alert(`Couldn't able to get your location`);
+//       }
+//     );
+//   }
+// });
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+const imgContainer = document.querySelector('.images');
+
+const wait = second => {
+  return new Promise(resolve => setTimeout(resolve, second * 1000));
+};
+let currentImage;
+
+const createImage = imgPath => {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', () => {
+      imgContainer.append(img);
+      resolve(img);
     });
+
+    img.addEventListener('error', () => {
+      console.log(`Image not found at given path : ${imgPath}`);
+    });
+  });
 };
 
-btn.addEventListener('click', e => {
-  e.preventDefault();
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        console.log(position);
-        const { latitude, longitude } = position.coords;
-        console.log(latitude, longitude);
-
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-
-        whereAmI(latitude, longitude);
-      },
-      function () {
-        alert(`Couldn't able to get your location`);
-      }
-    );
-  }
-});
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImage = img;
+    console.log('Image 1 loaded!');
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImage = img;
+    console.log('Image 2 loaded!');
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+  })
+  .catch(err => console.error(err));
